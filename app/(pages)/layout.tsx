ecee96f568ab667f3d6eef74dfd3eda793a9837d
srikanthpula341 <pulasrikanth1999@gmail.com>
2026-03-30 16:52:08 +0530

    lading page completed

---
+++ app/(pages)/layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function PagesLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+
+
+
+  return (
+    <>
+      <>{children}</>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
-import Image from "next/image";
+import LandingComponent from "./components/landing";
+import FooterComponent from "./components/footer";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-grow">
+        <LandingComponent />
+      </main>
     </div>
   );
 }
